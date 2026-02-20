b36c526049425b915398ecee0b70d4b9ba629d2a
Raechel <raechelkelishiya@gmail.com>
2026-02-20 19:33:44 +0530

    fixed root url loading issue

--- dental/urls.py
+++ dental/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 from presentation import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('presentation.urls')),
+    path('',views.homepage,name='home'),
+    path('hers/',views.hers,name='hers'),
+    path('hert/',views.hert,name='hert'),
+    path('thank/',views.thank,name='thank'),
+
+
 ]
